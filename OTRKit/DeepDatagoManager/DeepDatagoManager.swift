c7c88de67e8110021f9f541116539cf3cfec3d1a
Mingjun <tnndtnnd@gmail.com>
2018-08-20 21:57:37 -0400

    send add friend request POST method

--- OTRKit/DeepDatagoManager/DeepDatagoManager.swift
+++ OTRKit/DeepDatagoManager/DeepDatagoManager.swift
@@ -34,12 +34,10 @@ let TAG_SENDER_ADDRESS = "sender_address"
         }
         
         let accounts = (keyStore.getAccounts())!
-        var newAccount: GethAccount
-        if (accounts.size() <= 0) {
+        // var newAccount: GethAccount
+        var newAccount = getAccount()
+        if (newAccount == nil) {
             newAccount = createUser(ks:keyStore, password:password as String)
-        }
-        else {
-            newAccount = try! accounts.get(0)
         }
         
         var publicKeyPEM:NSString = ""
@@ -54,33 +52,81 @@ let TAG_SENDER_ADDRESS = "sender_address"
         }
         try! keyStore.unlock(newAccount, passphrase: password as String)
 
-        let registerRequestStr = createRegisterRequest(ks:keyStore, account:newAccount, nickName:nickName as String, publicKeyPEM:publicKeyPEM as String)!
+        let registerRequestStr = createRegisterRequest(ks:keyStore, account:newAccount!, nickName:nickName as String, publicKeyPEM:publicKeyPEM as String)!
         // print("register request: \((registerRequestStr))")
 
 
         return registerRequestStr as NSString;
     }
 
+    private func getAccount() -> GethAccount! {
+        let accounts = (keyStore.getAccounts())!
+        var newAccount: GethAccount
+        if (accounts.size() <= 0) {
+            return nil
+        }
+        else {
+            newAccount = try! accounts.get(0)
+        }
+        return newAccount
+    }
+    
     private func getPublicKeyRequest(account:String) -> String! {
-        let url = URL(string: BASEURL + "accounts/get_public_key/" + account + "/")
-        var request = URLRequest(url: url!)
-        request.httpMethod = "GET"
-        var responseString = ""
+        let data = sendGETRequest(urlString:(BASEURL + "accounts/get_public_key/" + account + "/"))
+        if (data == nil) {
+            return ""
+        }
+        let responseString = String(data: data!, encoding: String.Encoding.utf8)!
+        let jsonData = responseString.data(using: .utf8)!
         do {
-            let response: AutoreleasingUnsafeMutablePointer<URLResponse?>? = nil
-            let data = try NSURLConnection.sendSynchronousRequest(request, returning:response)
-            responseString = String(data: data, encoding: String.Encoding.utf8)!
-            let jsonData = responseString.data(using: .utf8)!
             let jsonArray = try JSONSerialization.jsonObject(with: jsonData, options : .allowFragments) as? Dictionary<String,Any>
             let publicKeyStr = (jsonArray!["publicKey"])!
             return publicKeyStr as! String;
-            // let newResponse = (response?.pointee)!
-            // let responseData = String(data: (response?.pointee)!, encoding: NSUTF8StringEncoding)
         } catch let error as NSError{
             print (error.localizedDescription)
-            return "";
         }
-        return responseString;
+        return ""
+    }
+
+    private func sendPOSTRequest(urlString:String, input:String) -> Data! {
+        print(urlString)
+        let url = URL(string: urlString)
+        var request = URLRequest(url: url!)
+        request.httpMethod = "POST"
+        let inputData = input.data(using: .utf8)!
+        request.httpBody = inputData
+        request.setValue("application/json", forHTTPHeaderField: "ContentType")
+        do {
+            let response: AutoreleasingUnsafeMutablePointer<URLResponse?>? = nil
+            var response2: URLResponse?
+            let data = try NSURLConnection.sendSynchronousRequest(request, returning:&response2)
+            if ((response2! as! HTTPURLResponse).statusCode != 200) {
+                return nil
+            }
+            return data;
+        } catch let error as NSError{
+            print (error.localizedDescription)
+        }
+        return nil;
+    }
+
+    private func sendGETRequest(urlString:String) -> Data! {
+        let url = URL(string: urlString)
+        var request = URLRequest(url: url!)
+        request.httpMethod = "GET"
+        do {
+            let response: AutoreleasingUnsafeMutablePointer<URLResponse?>? = nil
+            var response2: URLResponse?
+            let data = try NSURLConnection.sendSynchronousRequest(request, returning:&response2)
+            if ((response2! as! HTTPURLResponse).statusCode != 200) {
+                return nil
+            }
+
+            return data;
+        } catch let error as NSError{
+            print (error.localizedDescription)
+        }
+        return nil;
     }
 
     @objc public func getAddFriendRequest(account:NSString) -> NSString! {
@@ -95,8 +141,27 @@ let TAG_SENDER_ADDRESS = "sender_address"
         var requestData: NSMutableDictionary = NSMutableDictionary()
         requestData.setValue(encryptedKeyForFriend, forKey:TAG_FRIEND_REQUEST_SYMMETRIC_KEY)
         requestData.setValue(encryptedKeyForAllFriends, forKey:TAG_ALL_FRIENDS_SYMMETRIC_KEY)
+        let keysRequestData = try! JSONSerialization.data(withJSONObject: requestData, options: JSONSerialization.WritingOptions())
+        // let keysRequestString = NSString(data: keysRequestData as Data, encoding: String.Encoding.utf8.rawValue) as! String
+        // let encrpytedKeyStrData = encryptedKeysStr.data(using: .utf8)!
+        let gethAccount = getAccount()
+        let transactionStr = signTransaction(ks: keyStore, account: gethAccount!, data: keysRequestData)
 
-        return nil;
+        var friendRequest: NSMutableDictionary = NSMutableDictionary()
+        friendRequest.setValue(0, forKey:"action_type")
+        friendRequest.setValue("0x" + (account as String), forKey:"to_address")
+        friendRequest.setValue(gethAccount?.getAddress().getHex(), forKey:"from_address")
+        friendRequest.setValue(transactionStr, forKey:"request")
+        let friendRequestData = try! JSONSerialization.data(withJSONObject: friendRequest, options: JSONSerialization.WritingOptions()) as NSData
+        let friendRequestDataString = NSString(data: friendRequestData as Data, encoding: String.Encoding.utf8.rawValue) as! String
+        
+        let data = sendPOSTRequest(urlString:(BASEURL + "request/friend/"), input: friendRequestDataString);
+        if (data == nil) {
+            return ""
+        }
+        let responseString = String(data: data!, encoding: String.Encoding.utf8)!
+
+        return "";
     }
     
     private func signTransaction(ks: GethKeyStore, account:GethAccount, data: Data) -> String {
@@ -109,6 +174,7 @@ let TAG_SENDER_ADDRESS = "sender_address"
         
         // Sign a transaction with multiple manually cancelled authorizations
         // try! ks.unlock(account, passphrase: password)
+        try! ks.unlock(account, passphrase: "abc")
         let signed = try! ks.signTx(account, tx: tx, chainID: chain)
         let signedTrans = try! signed.encodeJSON()
         return signedTrans
